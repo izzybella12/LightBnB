0e5a31a98be8624d7b736fd61e2fcc061fb4c494
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-30 19:18:50 +0000

    Implement reservations database connection with front end

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -81,7 +81,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id  = properties.id 
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date DESC
+  `, [guest_id])
+  .then (res => res.rows)
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
